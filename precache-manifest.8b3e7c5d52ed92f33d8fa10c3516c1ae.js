4c9a6b7a36137c434d67cdb45704f0cce1c5366c
jithender04 <jithenderjupally@gmail.com>
2021-09-15 00:24:46 +0530

    Updates

---
+++ precache-manifest.8b3e7c5d52ed92f33d8fa10c3516c1ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a05fc6e98f8bf7cbf010bfe68672da",
+    "url": "/fiddle/index.html"
+  },
+  {
+    "revision": "847e9a8067d17055b19f",
+    "url": "/fiddle/static/css/2.21216002.chunk.css"
+  },
+  {
+    "revision": "d50916e2d1ae87c13b10",
+    "url": "/fiddle/static/css/main.6cfc4c50.chunk.css"
+  },
+  {
+    "revision": "847e9a8067d17055b19f",
+    "url": "/fiddle/static/js/2.784606ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fiddle/static/js/2.784606ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50916e2d1ae87c13b10",
+    "url": "/fiddle/static/js/main.19f31485.chunk.js"
+  },
+  {
+    "revision": "56a477da69448260ff6c",
+    "url": "/fiddle/static/js/runtime-main.f3dceab1.js"
+  }
+]);
